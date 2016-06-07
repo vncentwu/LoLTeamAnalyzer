eb5d08573424f32aaa83eef190fefcf113e09406
vncentwu <vncentwu@gmail.com>
2016-06-07 11:40:41 -0500

    Updated apperance

--- Script.js
+++ Script.js
@@ -1,17 +1,5 @@
 var API_KEY = "cd45f090-7b54-4f95-99ea-7e291c3263e2";
-
-/*var SUMMONER_NAME = "";
-var SUMMONER_NAME_2 = "";
-var SUMMONER_NAME_3 = "";
-var SUMMONER_NAME_4 = "";
-var SUMMONER_NAME_5 = "";
-var champName = "";
-var champName_2 = "";
-var champName_3 = "";
-var champName_4 = "";
-var champName_5 = "";*/
-
-var champIDs;
+var champIDs; //static list of champion ids
 var summonerNames = ["none", "none", "none", "none", "none"];
 var champNames = ["none", "none", "none", "none", "none"];
 var IDs = [0, 0, 0, 0, 0];
@@ -22,49 +10,60 @@ function summonerLookUp() {
 	
 	var SUMMONER_NAME;
 	var i;
-	for(i = 1; i < 6; i++)
+	for(i = 1; i < 11; i++)
 	{
 		summonerNames[i-1] = $("#userName" + i).val();
 		champNames[i-1] = $("#championPlayed" + i).val();
 	}
 	getStatics();
+	
 	var j;
-	for(j = 0; j < 5; j++)
+	var SUMMONER_NAMES = "";
+	for(j = 0; j < 11; j++)
 	{
 		SUMMONER_NAME = summonerNames[j];
-		
-	    if (SUMMONER_NAME !== "") {
-			$.ajax({
-				async: false,
-				url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + SUMMONER_NAME + '?api_key=' + API_KEY,
-				type: 'GET',
-				dataType: 'json',
-				data: {
-
-				},
-				success: function (json) {
-					var SUMMONER_NAME_NOSPACES = SUMMONER_NAME.replace(" ", "");
-					SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
-					levels[j] = json[SUMMONER_NAME_NOSPACES].summonerLevel;				
-					IDs[j] = json[SUMMONER_NAME_NOSPACES].id;
-					document.getElementById("sLevel" + (j+1)).innerHTML = levels[j];
-					playerStats(j);
-				},
-				error: function (XMLHttpRequest, textStatus, errorThrown) {
-					alert("error getting Summoner data!");
-				}
-			});
-    } else {}  	
+		if(SUMMONER_NAME !== "")
+		{
+			SUMMONER_NAMES = SUMMONER_NAMES + SUMMONER_NAME + ",";
+		}
 	}
+	$.ajax({
+		async: false,
+		url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + SUMMONER_NAMES + '?api_key=' + API_KEY,
+		type: 'GET',
+		dataType: 'json',
+		data: {
 
+		},
+		success: function (json) {
+			var n;
+			for(n = 0; n < 10; n++)
+			{
+				var SUMMONER_NAME_NOSPACES = summonerNames[n].replace(" ", "");
+				SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
+				levels[n] = json[SUMMONER_NAME_NOSPACES].summonerLevel;				
+				IDs[n] = json[SUMMONER_NAME_NOSPACES].id;
+				document.getElementById("sLevel" + (n+1)).innerHTML = levels[n];
+				playerStats(n);				
+			}
+
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+			alert("error getting Summoner data!");
+		}
+	});
 }
 
 function playerStats(i) {
 	
 	var champID = 0;
 	if(champNames[i] !== "")
-		champID = champIDs[champNames[i]].id;
-	
+	{
+		var name = champNames[i].replace(" ", "");
+		name = name.toLowerCase().trim();
+		name = name.charAt(0).toUpperCase() + name.slice(1);
+		champID = champIDs[name].id;
+	}
     $.ajax({
         url: "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + IDs[i] + "/ranked?season=SEASON2015&api_key=" + API_KEY,
         type: 'GET',
@@ -85,17 +84,14 @@ function playerStats(i) {
 					stats = champStats.stats;
 					wins = stats.totalSessionsWon;
 					losses = stats.totalSessionsLost;
-					winrate = wins/(losses+wins);
+					winrate = Math.round(wins * 100/(losses+wins)) + "%";
 					gamesPlayed = wins + losses;
 					document.getElementById("totalGames" + (i+1)).innerHTML = gamesPlayed;
 					document.getElementById("winRate" + (i+1)).innerHTML = winrate;
 				}
 					
 			} 
-            
-
         },
-
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             alert("error getting Summoner data 2!");
         }
@@ -103,8 +99,6 @@ function playerStats(i) {
 }
 
 function getStatics() {
-	
-	//alert("hello");
     $.ajax({
 		async: false,
         url: "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=" + API_KEY, 
@@ -114,10 +108,8 @@ function getStatics() {
 
         },
         success: function (resp) {
-			
 			champIDs = resp["data"];
         },
-
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             alert("error getting Summoner data 2!");
         }
